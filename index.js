16fec496a2317ad24d3e5783dfb2663af7c9db1f
IkramovAmir <amirikromov30@gmail.com>
2025-03-21 12:45:04 +0500

    do

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// 1. Katta harfga o‘girish
 document.querySelector(".uppercase-btn").onclick = function() {
     let input = document.querySelector(".text-input");
     document.querySelector(".result").innerText = input.value.toUpperCase();
 }
 
-// 2. Raqamlar yig‘indisini hisoblash
 // enter number example: 2,3,4
 document.querySelector(".sum-btn").onclick = function() {
     let input = document.querySelector(".sum-input").value;
@@ -16,7 +14,6 @@ document.querySelector(".sum-btn").onclick = function() {
     document.querySelector(".sum-result").innerText = "Yig‘indi: " + sum;
 }
 
-// 3. Ro‘yxatdan element o‘chirish
 let deleteButtons = document.querySelectorAll(".delete-btn");
 for (let i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = function() {
@@ -24,7 +21,6 @@ for (let i = 0; i < deleteButtons.length; i++) {
     }
 }
 
-// 4. Parol kuchliligini tekshirish
 document.querySelector(".check-password-btn").onclick = function() {
     let password = document.querySelector(".password-input").value;
     let result = "";
@@ -38,7 +34,6 @@ document.querySelector(".check-password-btn").onclick = function() {
     document.querySelector(".password-result").innerText = result;
 }
 
-// 5. FizzBuzz o‘yini
 document.querySelector(".fizzbuzz-btn").onclick = function() {
     let num = Number(document.querySelector(".fizzbuzz-input").value);
     let result = "";
@@ -54,7 +49,6 @@ document.querySelector(".fizzbuzz-btn").onclick = function() {
     document.querySelector(".fizzbuzz-result").innerText = result;
 }
 
-// 6. Timer (0 dan 10 gacha)
 document.querySelector(".timer-btn").onclick = function() {
     let count = 0;
     let timer = setInterval(function() {
@@ -66,7 +60,6 @@ document.querySelector(".timer-btn").onclick = function() {
     }, 1000);
 }
 
-// 7. Karra jadvali
 document.querySelector(".table-btn").onclick = function() {
     let num = Number(document.querySelector(".table-input").value);
     let result = "";
